22f1887d505702a1fa2b4905d432b8a223e41551
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-21 01:07:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+
+  end
+
 end
